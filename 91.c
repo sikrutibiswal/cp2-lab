5b49386848b7bc89350248e41189a358ca782da6
sikruti biswal <sikrutibiswal5@gmail.com>
2026-05-01 14:18:58 +0530

    Create 91.c

---
+++ 91.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x = 5, y = 10;
+    swap(&x, &y);
+    printf("After swap: %d %d", x, y);
+    return 0;
+}
